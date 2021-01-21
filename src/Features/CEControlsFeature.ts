a03530f8a8bb1b771723ca0b61b3cc393f0ab561
Dimnez <marner.daniel@googlemail.com>
2021-01-21 20:20:55 +0100

    frontend: add mouse event handling

--- src/Features/CEControlsFeature.ts
+++ src/Features/CEControlsFeature.ts
@@ -2,54 +2,69 @@ import CEFeatureBase from './CEFeatureBase';
 import CEConfiguration from '../CEConfiguration';
 
 export enum KeyboardKeys {
-	LEFT = 39,
-	RIGHT = 37,
-	UP = 38,
-	DOWN = 40,
-	ENTER = 13,
-	SPACE = 32,
-	ESCAPE = 27,
-	SHIFT = 16,
+    LEFT = 39,
+    RIGHT = 37,
+    UP = 38,
+    DOWN = 40,
+    ENTER = 13,
+    SPACE = 32,
+    ESCAPE = 27,
+    SHIFT = 16,
 };
 
-class CEControlsFeature extends CEFeatureBase
-{
-    public currentKeys : Set<string> = new Set();
+interface MouseState {
+    clientX: number,
+    clientY: number,
+    mouseDown: boolean
+}
 
-    constructor(configuration : CEConfiguration)
-    {
+class CEControlsFeature extends CEFeatureBase {
+    public currentKeys: Set<string> = new Set();
+    public mouseState: MouseState = { clientX: 0, clientY: 0, mouseDown: false };
+
+    constructor(configuration: CEConfiguration) {
         super(configuration);
         this.setEvents(configuration.window!);
     }
 
-    initialize()
-    {
+    initialize() {
         this.initialized = true;
     }
 
-    setEvents(window: Window)
-    {
+    setEvents(window: Window) {
         window.addEventListener("keydown", this.keyDown.bind(this), false);
         window.addEventListener("keyup", this.keyUp.bind(this), false);
+        window.addEventListener("mousemove", this.mousemove.bind(this), false);
+        window.addEventListener("mousedown", this.mousedown.bind(this), false);
+        window.addEventListener("mouseup", this.mouseup.bind(this), false);
     }
 
-    keyDown(event : KeyboardEvent)
-    {
+    keyDown(event: KeyboardEvent) {
         this.currentKeys.add(event.code);
     }
 
-    keyUp(event : KeyboardEvent)
-    {
+    keyUp(event: KeyboardEvent) {
         this.currentKeys.delete(event.code);
     }
 
-    read()
-    {
+    mousemove(event: MouseEvent) {
+        this.mouseState.clientX = event.clientX;
+        this.mouseState.clientY = event.clientY;
+    }
+
+    mousedown(event: MouseEvent) {
+        this.mouseState.mouseDown = true;
+    }
+
+    mouseup(event: MouseEvent) {
+        this.mouseState.mouseDown = false;
+    }
+
+    read() {
         return this.currentKeys;
     }
 
-    isPressed(key : KeyboardKeys)
-    {
+    isPressed(key: KeyboardKeys) {
         return this.currentKeys.has(key.toString());
     }
 
